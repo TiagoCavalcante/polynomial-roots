8025d9f486fb67388e53ed7fbd952a58ca60ebaa
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2022-07-18 22:28:32 -0300

    Write tests for crate::math

--- src/math.rs
+++ src/math.rs
@@ -4,3 +4,21 @@ pub fn is_zero(number: f32) -> bool {
   return -crate::constants::EPSILON < number
     && number < crate::constants::EPSILON;
 }
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn zero() {
+    assert_eq!(crate::math::is_zero(0.0), true);
+  }
+
+  #[test]
+  fn negative_number() {
+    assert_eq!(crate::math::is_zero(-10.0), false);
+  }
+
+  #[test]
+  fn positive_number() {
+    assert_eq!(crate::math::is_zero(10.0), false);
+  }
+}
